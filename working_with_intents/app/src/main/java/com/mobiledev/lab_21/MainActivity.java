835cf4aae50fb1a81d0bd30e05d2d536660111bb
mikepierre <mikeypierre84@yahoo.com>
2015-08-25 10:07:17 -0400

    Working with Intents
    This tutorial shows you how to move from one intent to another with a
    click of a button.

---
+++ working_with_intents/app/src/main/java/com/mobiledev/lab_21/MainActivity.java
@@ -0,0 +1,49 @@
+package com.mobiledev.lab_21;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.*;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToGreen(View v)
+    {
+        Intent goGreen;
+        goGreen = new Intent(this, GreenActivity.class);
+        startActivity(goGreen);
+        Log.d("Hey Mikey","The Button is green, its green");
+    }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
+}
